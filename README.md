65767f3e4157b89fc3d39fea2fc56c05618f13c6
najmussakib2 <nnsnajmussakib@gmail.com>
2023-07-06 16:01:09 +0600

    project completed

---
+++ README.md
@@ -0,0 +1,13 @@
+## Installation
+
+For install dependency:
+
+```sh
+npm install
+```
+
+For run the project:
+
+```sh
+nodemon index.js
+```
---
+++ index.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u8fp9ke.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+
+    const toyDB = client.db("toyDB");
+    const toys = toyDB.collection("toys");
+
+    // Get all toy
+    app.get("/toys", async (req, res) => {
+      const currentPage = parseInt(req.query.currentPage) || 0;
+      const itemsPerPage = parseInt(req.query.itemsPerPage) || 20;
+
+      const skip = currentPage * itemsPerPage;
+      const result = await toys.find().skip(skip).limit(itemsPerPage).toArray();
+
+      res.send(result);
+    });
+
+    // Get total toys number
+    app.get("/totalToys", async (req, res) => {
+      const result = await toys.estimatedDocumentCount();
+      res.send({ totalToysNumber: result });
+    });
+
+    // Get specific user toy
+    app.get("/my-toys", async (req, res) => {
+      const userEmail = req.query.email;
+      const sortBy = req.query.sortBy;
+      const query = { seller_email: userEmail };
+
+      if (sortBy === "ascending") {
+        const cursor = toys.find(query).sort({ price: 1 });
+        const result = await cursor.toArray();
+        res.send(result);
+      } else if (sortBy === "descending") {
+        const cursor = toys.find(query).sort({ price: -1 });
+        const result = await cursor.toArray();
+        res.send(result);
+      } else {
+        const cursor = toys.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      }
+    });
+
+    // Get specific toy by id
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toys.findOne(query);
+      res.send(result);
+    });
+
+    // Get toy from search field
+    app.get("/toys-search", async (req, res) => {
+      const searchText = req.query.searchText;
+      const searchRegex = new RegExp(searchText, "i");
+      const result = await toys.find({ name: searchRegex }).toArray();
+      res.send(result);
+    });
+
+    // Get toys by category
+    app.get("/toys-subCat", async (req, res) => {
+      const subCat = req.query.subCat;
+      const query = { sub_category: subCat };
+      const cursor = toys.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Add a toy
+    app.post("/toys", async (req, res) => {
+      const toyInfo = req.body;
+      const doc = {
+        ...toyInfo,
+      };
+      const result = await toys.insertOne(doc);
+      res.send(result);
+    });
+
+    // Update toy
+    app.patch("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const toyInfo = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          ...toyInfo,
+        },
+      };
+      const result = await toys.updateOne(filter, updateDoc);
+      if (result.modifiedCount > 0) {
+        console.log("Successfully updated one document.");
+      } else {
+        console.log("No documents matched the query. Updated 0 documents.");
+      }
+      res.send(result);
+    });
+
+    // Delete a toy
+    app.delete("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toys.deleteOne(query);
+      if (result.deletedCount === 1) {
+        console.log("Successfully deleted one document.");
+      } else {
+        console.log("No documents matched the query. Deleted 0 documents.");
+      }
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server running...");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+
+
